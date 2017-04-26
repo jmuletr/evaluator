927458377161d47244f937f83e4fc829ab188662
jmuletr <jmuletr@gmail.com>
2017-04-26 22:19:22 +0200

    equals fet, getTokens falta tenir en compte arrels que no se quin signe es

--- src/Token.java
+++ src/Token.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Token {
     enum Toktype {
         NUMBER, OP, PAREN
@@ -46,11 +49,53 @@ public class Token {
 
     // Mètode equals. Comprova si dos objectes Token són iguals
     public boolean equals(Object o) {
+        Token token;
+        if (o instanceof Token){
+            token = (Token) o;
+        }else return false;
+        if (token.ttype != this.ttype) return false;
+        if (token.ttype == Toktype.NUMBER) {
+            if (token.value == this.value){
+                return true;
+            }else return false;
+        }
+        if (token.ttype == Toktype.OP) {
+            if (token.tk == this.tk){
+                return true;
+            }else return false;
+        }
+        if (token.ttype == Toktype.PAREN) {
+            if (token.tk == this.tk) {
+                return true;
+            }else return false;
+        }
         return false;
     }
 
     // A partir d'un String, torna una llista de tokens
     public static Token[] getTokens(String expr) {
-        return null;
+        List<Token> tokens = new ArrayList<>();
+        for (int i = 0; i < expr.length(); i++) {
+            String numero = "";
+            while (expr.charAt(i) >= '0' && expr.charAt(i) <= '9') {
+                numero += expr.charAt(i);
+                if (i == expr.length() - 1) {
+                    break;
+                } else {
+                    i++;
+                }
+            }
+            if (numero.length() > 0) {
+                tokens.add(tokNumber(Integer.parseInt(numero)));
+            }
+            if (expr.charAt(i) == '+' || expr.charAt(i) == '-' || expr.charAt(i) == '*' || expr.charAt(i) == '/' || expr.charAt(i) == '^' ) {
+                tokens.add(tokOp(expr.charAt(i)));
+            } else if (expr.charAt(i) == '(' || expr.charAt(i) == ')') {
+                tokens.add(tokParen(expr.charAt(i)));
+            }
+        }
+        Token[] token = new Token[tokens.size()];
+        tokens.toArray(token);
+        return token;
     }
 }
